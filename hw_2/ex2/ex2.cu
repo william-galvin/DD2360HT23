9562d7cee5f3a594fc2aa58aef6aaec63d9ba6aa
Ricardo G <78384733+rgg2001@users.noreply.github.com>
2023-11-19 22:25:39 +0100

    Update ex2.cu
    Getting issue with comparison failed

--- hw_2/ex2/ex2.cu
+++ hw_2/ex2/ex2.cu
@@ -1,66 +1,123 @@
-
 #include <stdio.h>
 #include <sys/time.h>
+#include <cstdlib>
+#include <time.h>
 
 #define DataType double
+#define TPB 32
+#define EPS 0.00001
 
-// Compute C = A * B
-__global__ void gemm(DataType *A, DataType *B, DataType *C, int numARows,
-                      int numAColumns, int numBRows, int numBColumns){
-  //@@ Insert code to implement matrix multiplication here
+__global__ void matMul(DataType* A, DataType* B, DataType* C, int numARows, int numAColumns, int numBRows, int numBColumns) {
+    int row = blockIdx.y * blockDim.y + threadIdx.y;
+    int col = blockIdx.x * blockDim.x + threadIdx.x;
 
+    if (row < numARows && col < numBColumns) {
+        DataType sum = 0.0;
+        for (int k = 0; k < numAColumns; k++) {
+            sum += A[row * numAColumns + k] * B[k * numBColumns + col];
+        }
+        C[row * numBColumns + col] = sum;
+    }
 }
 
-int main(int argc, char **argv) {
-  
-  DataType *hostA; // The A matrix
-  DataType *hostB; // The B matrix
-  DataType *hostC; // The output C matrix
-  DataType *resultRef; // The reference result
-  DataType *deviceA;
-  DataType *deviceB;
-  DataType *deviceC;
-  int numARows;    // number of rows in the matrix A
-  int numAColumns; // number of columns in the matrix A
-  int numBRows;    // number of rows in the matrix B
-  int numBColumns; // number of columns in the matrix B
-  int numCRows;
-  int numCColumns;
+DataType randData(DataType min, DataType max) {
+  DataType range = (max - min);
+  DataType div = RAND_MAX / range;
+  return min + (rand() / div);
+}
 
-  //@@ Insert code below to read in numARows, numAColumns, numBColumns from args
+int main(int argc, char** argv) {
 
-  printf("Input matrix dim (%d x %d) (%d x %d) (%d x %d)\n", numARows, numAColumns, numBRows, numBColumns, numCRows, numCColumns);
-  
-  //@@ Insert code below to allocate Host memory for input and output
+    if (argc != 3) {
+      printf("expected three arguments, found %d\n", argc);
+      exit(-1);
+    }
 
-  
-  //@@ Insert code below to initialize hostA and hostB to random numbers, and create reference result in CPU
+    DataType* hostInputA;
+    DataType* hostInputB;
+    DataType* hostOutput;
+    DataType* resultRef;
+    DataType* deviceInputA;
+    DataType* deviceInputB;
+    DataType* deviceOutput;
 
+    int numARows;    // number of rows in the matrix A
+    int numAColumns; // number of columns in the matrix A
+    int numBRows;    // number of rows in the matrix B
+    int numBColumns; // number of columns in the matrix B
 
-  //@@ Insert code below to allocate GPU memory here
+    numARows = atoi(argv[1]);
+    numAColumns = atoi(argv[2]);
+    numBRows = numAColumns;
+    numBColumns = numARows;
 
+    printf("%d,", numARows * numAColumns);
 
-  //@@ Insert code to below to Copy memory to the GPU here
+    //@@ Insert code below to allocate Host memory for input and output
+    hostInputA = (DataType*)malloc(sizeof(DataType) * numARows * numAColumns);
+    hostInputB = (DataType*)malloc(sizeof(DataType) * numBRows * numBColumns);
+    hostOutput = (DataType*)malloc(sizeof(DataType) * numARows * numBColumns);
 
+    //@@ Insert code below to initialize hostInputA and hostInputB to random numbers, and create reference result in CPU
+    srand(time(0));
+    resultRef = (DataType*)malloc(sizeof(DataType) * numARows * numBColumns);
+    for (int i = 0; i < numARows; i++) {
+        for (int j = 0; j < numBColumns; j++) {
+            hostOutput[i * numBColumns + j] = 0;
+            for (int k = 0; k < numAColumns; k++) {
+                hostInputA[i * numAColumns + k] = randData(0, 100);
+                hostInputB[k * numBColumns + j] = randData(0, 100);
+                resultRef[i * numBColumns + j] += hostInputA[i * numAColumns + k] * hostInputB[k * numBColumns + j];
+            }
+        }
+    }
 
-  //@@ Initialize the grid and block dimensions here
+    //@@ Insert code below to allocate GPU memory here
+    cudaMalloc(&deviceInputA, sizeof(DataType) * numARows * numAColumns);
+    cudaMalloc(&deviceInputB, sizeof(DataType) * numBRows * numBColumns);
+    cudaMalloc(&deviceOutput, sizeof(DataType) * numARows * numBColumns);
 
+    //@@ Insert code to below to Copy memory to the GPU here
+    clock_t start = clock();
+    cudaMemcpy(deviceInputA, hostInputA, sizeof(DataType) * numARows * numAColumns, cudaMemcpyHostToDevice);
+    cudaMemcpy(deviceInputB, hostInputB, sizeof(DataType) * numBRows * numBColumns, cudaMemcpyHostToDevice);
+    cudaMemcpy(deviceOutput, hostOutput, sizeof(DataType) * numARows * numBColumns, cudaMemcpyHostToDevice);
+    printf("%ld,", clock() - start);
 
-  //@@ Launch the GPU Kernel here
+    //@@ Initialize the 2D grid and block dimensions here
+    dim3 threadsPerBlock(TPB, TPB);
+    dim3 blocksPerGrid((numBColumns + TPB - 1) / TPB, (numARows + TPB - 1) / TPB);
 
+    //@@ Launch the GPU Kernel here
+    start = clock();
+    matMul<<<blocksPerGrid, threadsPerBlock>>>(deviceInputA, deviceInputB, deviceOutput, numARows, numAColumns, numBRows, numBColumns);
+    cudaDeviceSynchronize();
+    printf("%ld,", clock() - start);
 
-  //@@ Copy the GPU memory back to the CPU here
+    //@@ Copy the GPU memory back to the CPU here
+    start = clock();
+    cudaMemcpy(hostOutput, deviceOutput, sizeof(DataType) * numARows * numBColumns, cudaMemcpyDeviceToHost);
+    printf("%ld,\n", clock() - start);
 
+    //@@ Insert code below to compare the output with the reference
+    for (int i = 0; i < numARows * numBColumns; i++) {
+      if (abs(resultRef[i] - hostOutput[i]) > EPS) {
+        printf("Comparison failed at index %d. Expected %f but got %f.\n", i, resultRef[i], hostOutput[i]);
+        exit(-1);
+      }
+    }
 
-  //@@ Insert code below to compare the output with the reference
+    //@@ Free the GPU memory here
+    cudaFree(deviceInputA);
+    cudaFree(deviceInputB);
+    cudaFree(deviceOutput);
 
+    //@@ Free the CPU memory here
+    free(hostInputA);
+    free(hostInputB);
+    free(hostOutput);
+    free(resultRef);
 
-  //@@ Free the GPU memory here
-
-
-  //@@ Free the CPU memory here
-
-
-  return 0;
+    return 0;
 }
 
